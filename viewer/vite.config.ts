bf53c3dda838740a2d1a3acc0e551bee808ed9ae
Github Action (authored by pmalacho-mit) <pmalacho-mit@users.noreply.github.com>
2024-02-04 22:07:43 -0800

    corrected path to work on github deploy

--- viewer/vite.config.ts
+++ viewer/vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
+console.log('process.env.NODE_ENV', process.env.NODE_ENV);
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: process.env.NODE_ENV === 'production'
+    ? 'prg-scratch-diff/'
+    : '/'
 })
